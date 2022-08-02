524ae8898d75ea6cbc61251be46ba8c49fd040ff
ada-k <adakibetj@protonmail.com>
2022-08-02 11:34:16 +0300

    gpt2, bert, transformer_xl, xlnet

---
+++ transformer_xl.py
@@ -0,0 +1,5 @@
+"""text generation command"""
+!python pytorch-transformers/examples/run_generation.py 
+    --model_type=transfo-xl 
+    --length=100 
+    --model_name_or_path=transfo-xl-wt103
---
+++ xlnet.py
@@ -0,0 +1,6 @@
+# terminal command for tedxt generation
+
+!python pytorch-transformers/examples/run_generation.py
+    --model_type=xlnet
+    --length=50
+    --model_name_or_path=xlnet-base-cased
